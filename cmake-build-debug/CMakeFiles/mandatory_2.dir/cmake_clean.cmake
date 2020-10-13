ba9a2f0bf85df20665a1ea427747492550d90c33
Thomas <thomas8@hohnen.dk>
2020-10-13 16:21:50 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/mandatory_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mandatory_2.dir/main.c.obj"
+  "libmandatory_2.dll.a"
+  "mandatory_2.exe"
+  "mandatory_2.exe.manifest"
+  "mandatory_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mandatory_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
